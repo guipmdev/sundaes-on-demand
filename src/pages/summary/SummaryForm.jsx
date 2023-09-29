06f94d66f0a93c88524461b20705b728de8fc68f
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-29 16:37:51 -0300

    SummaryForm Checkbox and Button

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,33 @@
+import { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+
 export function SummaryForm() {
-  return <div />
+  const [tcChecked, setTcChecked] = useState(false)
+
+  function handleChangeButtonStatus(event) {
+    setTcChecked(event.currentTarget.checked)
+  }
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+    </span>
+  )
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={handleChangeButtonStatus}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  )
 }
