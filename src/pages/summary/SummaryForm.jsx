a97e38d84fbfb9e169d348c35b2468b2b7808bac
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-29 17:32:56 -0300

    Popover

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
 
-export function SummaryForm() {
+export function SummaryForm({ setOrderPhase }) {
   const [tcChecked, setTcChecked] = useState(false)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase('completed')
+  }
 
   function handleChangeButtonStatus(event) {
     setTcChecked(event.currentTarget.checked)
   }
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  )
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   )
 
