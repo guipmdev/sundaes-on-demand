d07d77939d8a601b767409d8fd91eaaacdc4a738
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-01 11:37:42 -0300

    Using 'await findBy' to Find Elements that Populate Asynchronously

--- src/pages/Entry/ScoopOption.jsx
+++ src/pages/Entry/ScoopOption.jsx
@@ -5,7 +5,7 @@ export function ScoopOption({ name, imagePath }) {
     <Col xs={12} sm={6} md={4} ld={3} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
-        src={`http://localhost:3030/${imagePath}`}
+        src={`http://localhost:3030${imagePath}`}
         alt={`${name} scoop`}
       />
     </Col>
