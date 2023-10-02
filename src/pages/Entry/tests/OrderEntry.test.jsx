ab64da09f705edcadc7e2cc11d4b58b0304e04d9
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-02 09:36:27 -0300

    Running only Selected Tests, and 'waitFor'

--- src/pages/Entry/tests/OrderEntry.test.jsx
+++ src/pages/Entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { logRoles, render, screen, waitFor } from '@testing-library/react'
 import { OrderEntry } from '../OrderEntry'
 import { rest } from 'msw'
 import { server } from '../../../mocks/server'
@@ -6,18 +6,18 @@ import { server } from '../../../mocks/server'
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
-      res(ctx.json(500)),
+      res(ctx.status(500)),
     ),
     rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
-      res(ctx.json(500)),
+      res(ctx.status(500)),
     ),
   )
 
   render(<OrderEntry />)
 
-  const alerts = await screen.findAllByRole('alert', {
-    name: 'An unexpected error occurred. Please try again later.',
-  })
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole('alert')
 
-  expect(alerts).toHaveLength(2)
+    expect(alerts).toHaveLength(2)
+  })
 })
