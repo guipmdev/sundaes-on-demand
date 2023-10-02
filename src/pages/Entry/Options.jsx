d8072cfc7474d0f17d601bf6eaf4c13100b60ed9
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-02 13:30:07 -0300

    Use Context to Display Scoops Subtotal

--- src/pages/Entry/Options.jsx
+++ src/pages/Entry/Options.jsx
@@ -4,8 +4,13 @@ import Row from 'react-bootstrap/Row'
 import { ScoopOption } from './ScoopOption'
 import { ToppingOption } from './ToppingOption'
 import { AlertBanner } from '../common/AlertBanner'
+import { pricePerItem } from '../../constants'
+import { formatCurrency } from '../../utilities'
+import { useOrderDetails } from '../../contexts/OrderDetails'
 
 export function Options({ optionType }) {
+  const { totals } = useOrderDetails()
+
   const [items, setItems] = useState([])
   const [error, setError] = useState(false)
 
@@ -23,6 +28,8 @@ export function Options({ optionType }) {
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
 
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase()
+
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
@@ -31,5 +38,14 @@ export function Options({ optionType }) {
     />
   ))
 
-  return <Row>{optionItems}</Row>
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  )
 }
--- src/pages/Entry/ScoopOption.jsx
+++ src/pages/Entry/ScoopOption.jsx
@@ -1,6 +1,15 @@
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import { useOrderDetails } from '../../contexts/OrderDetails'
 
 export function ScoopOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails()
+
+  function handleChange(event) {
+    updateItemCount(name, parseInt(event.currentTarget.value), 'scoops')
+  }
+
   return (
     <Col xs={12} sm={6} md={4} ld={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +17,23 @@ export function ScoopOption({ name, imagePath }) {
         src={`http://localhost:3030${imagePath}`}
         alt={`${name} scoop`}
       />
+
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: '10px' }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: 'right' }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" style={{ textAlign: 'left' }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   )
 }
