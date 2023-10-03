967ad225f310884b8a7332b7a7a835ce9562db6a
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-03 14:05:55 -0300

    Manual Acceptance Testing

--- src/pages/Entry/ScoopOption.jsx
+++ src/pages/Entry/ScoopOption.jsx
@@ -11,7 +11,7 @@ export function ScoopOption({ name, imagePath }) {
   }
 
   return (
-    <Col xs={12} sm={6} md={4} ld={3} style={{ textAlign: 'center' }}>
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
         src={`http://localhost:3030${imagePath}`}
