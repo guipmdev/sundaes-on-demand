1d08f0303c972fc65c7dc2600d665cc9b0b65b14
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-03 09:34:44 -0300

    Toppings Checkboxes

--- src/pages/Entry/ToppingOption.jsx
+++ src/pages/Entry/ToppingOption.jsx
@@ -1,6 +1,14 @@
+import { Form, Row } from 'react-bootstrap'
 import Col from 'react-bootstrap/Col'
+import { useOrderDetails } from '../../contexts/OrderDetails'
 
 export function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails()
+
+  function handleChange(event) {
+    updateItemCount(name, +event.currentTarget.checked, 'toppings')
+  }
+
   return (
     <Col xs={12} sm={6} md={4} ld={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +16,10 @@ export function ToppingOption({ name, imagePath }) {
         src={`http://localhost:3030${imagePath}`}
         alt={`${name} topping`}
       />
+
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
   )
 }
