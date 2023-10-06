dcd06b79775a714661a6c4a2771a423c4831d970
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-06 10:59:13 -0300

    chore: trying to fix workflow error

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 
 import { OrderConfirmation } from './pages/confirmation/OrderConfirmation'
-import { OrderEntry } from './pages/entry/OrderEntry'
 import { OrderSummary } from './pages/summary/OrderSummary'
+import { OrderEntry } from './pages/entry/OrderEntry'
 
 import { OrderDetailsProvider } from './contexts/OrderDetails'
 
