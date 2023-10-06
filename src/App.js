f48142ff1c3f17ed866d58d21e6d07be772f075f
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-06 11:31:46 -0300

    chore: trying to fix workflow failed runs

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ export function App() {
   // orderPhase needs to be 'inProgress', 'review' or 'completed'
   const [orderPhase, setOrderPhase] = useState('inProgress')
 
-  // let Component = OrderEntry // default to order page
-  // switch (orderPhase) {
-  //   case 'inProgress':
-  //     Component = OrderEntry
-  //     break
-  //   case 'review':
-  //     Component = OrderSummary
-  //     break
-  //   case 'completed':
-  //     Component = OrderConfirmation
-  //     break
-  //   default:
-  // }
+  let Component = OrderEntry // default to order page
+  switch (orderPhase) {
+    case 'inProgress':
+      Component = OrderEntry
+      break
+    case 'review':
+      Component = OrderSummary
+      break
+    case 'completed':
+      Component = OrderConfirmation
+      break
+    default:
+  }
 
   return (
     <OrderDetailsProvider>
       <Container>
-        <OrderEntry setOrderPhase={setOrderPhase} />
+        <Component setOrderPhase={setOrderPhase} />
       </Container>
     </OrderDetailsProvider>
   )
