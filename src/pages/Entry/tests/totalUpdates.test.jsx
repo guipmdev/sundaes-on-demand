4d54b2e7b4cab1fe34a9113908582f34792f6400
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-10-06 13:08:38 -0300

    chore: trying to fix workflow failed runs

--- src/pages/Entry/tests/totalUpdates.test.jsx
+++ src/pages/Entry/tests/totalUpdates.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '../../../test-utils/testing-library-utils'
 import userEvent from '@testing-library/user-event'
-import { Options } from '../Options'
-import { OrderEntry } from '../OrderEntry'
+import { Options } from '../../entry/Options'
+import { OrderEntry } from '../../entry/OrderEntry'
 
 test('update scoop subtotal when scoops change', async () => {
   const user = userEvent.setup()
